36e2cd8ff0ff5f72845eb9f96c04bb218f925397
farhankemal97 <farhankemal97@gmail.com>
2024-04-23 01:50:37 +0700

    Update CountWords.java
    Create methods to search words with character M and words that are longer than 5 characters

--- src/com/csg/CountWords.java
+++ src/com/csg/CountWords.java
@@ -5,8 +5,39 @@ import java.util.List;
 
 public class CountWords {
 
+    public static int countWordsStartsWithCharacterM (List<String> words){
+        int result = 0;
+        for(String word : words){
+            if(word.startsWith("M") || word.startsWith("m")){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static List<String> countWordsLongerThan5Characters (List<String> words){
+        List<String> result = new ArrayList<>();
+        for(String word : words){
+            if(word.length() > 5){
+                result.add(word);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        List<String> words = new ArrayList<>();
+        words.add("Mango");
+        words.add("mandarin");
+        words.add("lychee");
+        words.add("mammoth");
+
+        int wordsWithM = countWordsStartsWithCharacterM(words);
+        List<String> wordsLongerThan5 = countWordsLongerThan5Characters(words);
+
+
+        System.out.println(wordsWithM);
+        System.out.println(wordsLongerThan5);
     }
 }
