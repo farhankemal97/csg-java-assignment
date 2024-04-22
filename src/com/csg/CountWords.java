cd1b207f12de948c82358dc5153fcac79d17df78
farhankemal97 <farhankemal97@gmail.com>
2024-04-23 01:38:40 +0700

    init project
    Initial Commit

---
+++ src/com/csg/CountWords.java
@@ -0,0 +1,12 @@
+package com.csg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+    }
+}
